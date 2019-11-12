8604820b27c6d4aaf2c1d040731a8b0e036574b2
Massoud Valipoor <massoudvalipoor@outlook.com>
2019-11-12 10:14:48 +0100

    first commit

---
+++ lib/models/task_data.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/foundation.dart';
+import 'package:todo_app_flutter/models/task.dart';
+import 'dart:collection';
+
+class TaskData extends ChangeNotifier {
+  List<Task> _tasks = [
+    Task(name: 'Buy milk'),
+    Task(name: 'Buy eggs'),
+    Task(name: 'Buy bread'),
+  ];
+
+  UnmodifiableListView<Task> get tasks {
+    return UnmodifiableListView(_tasks);
+  }
+
+  int get taskCount {
+    return _tasks.length;
+  }
+
+  void addTask(String newTaskTitle) {
+    if (newTaskTitle == null || newTaskTitle.isEmpty) {
+      return;
+    }
+    final task = Task(name: newTaskTitle);
+    _tasks.add(task);
+    notifyListeners();
+  }
+
+  void updateTask(Task task) {
+    task.toggleDone();
+    notifyListeners();
+  }
+
+  void deleteTask(Task task) {
+    _tasks.remove(task);
+    notifyListeners();
+  }
+}
